7431d658b905fecfbfb48cf2a73e853abeae30f3
Arkadiy <dragon_500@mail.ru>
2019-03-24 11:03:44 +0300

    add street light and omni light, and improvement code

--- Lab - AR/ViewController.swift
+++ Lab - AR/ViewController.swift
@@ -8,47 +8,57 @@
 
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     // MARK: - Private properties
-    var sceneHouseName = "art.scnassets/Building/lowpoly_house.dae"
-    var sceneCarName = "art.scnassets/Car/van 003a_2012.dae"
-    var sceneHuman1Name = "art.scnassets/Character/Cas-Sum_Man_RtStand_366.dae"
-    var sceneHuman2Name = "art.scnassets/Character2/7.scn"
-    var sceneStreetLightName = "art.scnassets/Street light/Street light.scn"
+    private enum SceneName: String {
+        
+        enum Character: String, CaseIterable {
+            
+            case human1 = "art.scnassets/Character/Cas-Sum_Man_RtStand_366.dae"
+            case human2 = "art.scnassets/Character2/7.scn"
+            
+        }
+        
+        case house = "art.scnassets/Building/lowpoly_house.dae"
+        case car = "art.scnassets/Car/van 003a_2012.dae"
+        case streetLight = "art.scnassets/Street light/Street light.scn"
+        case starship = "art.scnassets/Starship/NXResonance_BM.scn"
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
-        
+        #if DEBUG
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+        #endif
         
         // Create a new scene
-        let scene = SCNScene(named: sceneHouseName)!
+        let scene = SCNScene(named: SceneName.house.rawValue)!
         
         // Set the scene to the view
         sceneView.scene = scene
         
-        // Create car
+        // Create and add car to the scene
         addCarToScene()
         
-        // Create plane
+        // Create and add plane to the scene
         addPlaneToScene()
         
-        // Create character
+        // Create and add character to the scene
         addCharactersToScene()
         
-        // Create street light
-        addStreetLightToScene()
+        // Create and add street light and omni light to the scene
+        addStreetLightAndOmniLightToScene()
         
     }
     
+    // MARK: Life cicles
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -59,6 +69,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
@@ -66,79 +81,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    
     // MARK: Private methods
     
     // --- add car
     private func addCarToScene() {
-        let car = nodeFrom(sceneCarName)
+        let car = nodeFrom(SceneName.car.rawValue)
         sceneView.scene.rootNode.addChildNode(car)
     }
     
     // --- add grass
     private func addPlaneToScene() {
-        
         let plane = SCNNode(geometry: SCNPlane(width: 1.5, height: 1.5))
         plane.position = SCNVector3(0.35, 0, -0.35)
         plane.eulerAngles = SCNVector3(-(90 as Float).radians, -(45 as Float).radians, 0)
         plane.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grass.jpg")
         sceneView.scene.rootNode.addChildNode(plane)
-        
     }
     
     // --- add street light
-    private func addStreetLightToScene() {
-        
-        let lamp = nodeFrom(sceneStreetLightName)
+    private func addStreetLightAndOmniLightToScene() {
+        let lamp = nodeFrom(SceneName.streetLight.rawValue)
         sceneView.scene.rootNode.addChildNode(lamp)
-        
     }
     
-    // --- add human
+    // --- add humans
     private func addCharactersToScene() {
-        
-        addCharacterToScene()
-        addCharacter2ToScene()
-        
+        addHumansToScene()
     }
     
-    private func addCharacterToScene() {
-        let human = nodeFrom(sceneHuman1Name)
-        sceneView.scene.rootNode.addChildNode(human)
+    private func addHumansToScene() {
+        let humans = SceneName.Character.allCases.map { $0.rawValue }
+        humans.forEach { sceneView.scene.rootNode.addChildNode(nodeFrom($0)) }
     }
-    
-    private func addCharacter2ToScene() {
-        let human = nodeFrom(sceneHuman2Name)
-        sceneView.scene.rootNode.addChildNode(human)
-    }
-    
     
     private func nodeFrom(_ sceneFile: String) -> SCNNode {
         return SCNScene(named: sceneFile)!.rootNode
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
